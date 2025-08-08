df3ab2f3ff3eb812beb16b3ac29f61a168f9e65c
shnliiu <shannon.liu04@gmail.com>
2025-08-08 13:21:20 -0700

    Update config.py

--- tutorial/config.py
+++ tutorial/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "us-west-2")
-KNOWLEDGE_BASE_ID = "N3HQQKXUXV"
+AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "")
+KNOWLEDGE_BASE_ID = ""
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
